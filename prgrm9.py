5bd40c5828edb1ba4de2a84682751a60c142919e
rahmath234 <rahmathck381@gmail.com>
2026-07-25 02:27:57 -0700

    Add files via upload

---
+++ prgrm9.py
@@ -0,0 +1,12 @@
+import matplotlib.pyplot as plt
+rollnumbers = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
+marks = [22,87,5,43,56,75,55,54,11,20,61,5,79,31,27]
+plt.figure(figsize=(9,5))
+plt.scatter(rollnumbers,marks,color='crimson',s=60,edgecolors='black',label='students')
+plt .title("Students mark vs Rollnumber")
+plt.xlabel("Rollnumber")
+plt.ylabel("Marks Obtained")
+plt.yticks(range (0,20,2))
+plt.grid(True,linestyle = '--',alpha = 0.6)
+plt.legend()
+plt.show()
